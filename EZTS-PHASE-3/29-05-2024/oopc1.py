adbb3e9f7d3d319bee7b8742d43657750e6c2baf
Kothapelly Sai Sidhartha <kothapallysidhartha@gmail.com>
2024-05-29 11:40:59 +0530

    first commit

---
+++ EZTS-PHASE-3/29-05-2024/oopc1.py
@@ -0,0 +1,61 @@
+# class Student:
+#     def_init_(self,roll,name,phone,address):
+#         self.roll=roll
+#         self.name=name
+#         self.phone=phone
+#         self.address=address
+#     
+#     def displayInfo(self):
+#         print("Name :",self.name,"Address :",self.address)
+#   
+# st1=Student(1501,'Akhil',8908798202,'warangal')
+# st1.displayInfo()
+# st2=Student(1009,'Ruthwik',8679998202,'warangal')
+# st2.displayInfo()
+
+
+
+# 
+# class Student:
+#     def __init__(self, roll, name, phone, address):
+#         self.roll = roll
+#         self.name = name
+#         self.phone = phone
+#         self.address = address
+# 
+#     def displayInfo(self):
+#         print("Name:", self.name, "Address:", self.address)
+# 
+# st1 = Student(1501, 'Akhil', 8908798202, 'Warangal')
+# st1.displayInfo()
+# st2 = Student(1009, 'Ruthwik', 8679998202, 'Warangal')
+# st2.displayInfo()
+# 
+# 
+# 
+# 
+# class Test:
+#     def __init__(self, a="Hello World"):
+#         self.a = a
+# 
+#     def display(self):
+#         print(self.a)
+# 
+# obj = Test()
+# obj.display()
+# 
+# 
+# 
+# 
+# def get_last_element(tuple):
+#     return tuple[-1]
+# 
+# simple_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+# sorted_list = sorted(simple_list, key=get_last_element)
+# print("Result:", sorted_list)
+
+# #sort elements as per the length
+# li=input().split(",")
+# li=sorted(li,key=len)
+# print(li)
+    
