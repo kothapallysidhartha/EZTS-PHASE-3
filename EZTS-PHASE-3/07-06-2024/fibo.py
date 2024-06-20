5df46ce45edb0b34db39e5919e50f3a4b17eeb17
Kothapelly Sai Sidhartha <kothapallysidhartha@gmail.com>
2024-06-20 14:35:43 +0530

    patterns

--- EZTS-PHASE-3/07-06-2024/fibo.py
+++ EZTS-PHASE-3/07-06-2024/fibo.py
@@ -8,5 +8,55 @@
 #     return fibv[n]
 # print(fib(int(input())))
 
-l=list(input().split(" "))
-print(l)
+# l=list(input().split(" "))
+# print(l)
+
+
+
+# li = [15,3,6,12,8,5]
+# res=0
+# def summ(li, res=0):
+#     if li:
+#         res += li[0]
+#         #print("s")
+#         return summ(li[1:], res)
+#     else:
+#         return res
+# 
+# print(summ(li))
+
+# s = input()
+# 
+# def pal(s):
+#     if len(s) <= 1:
+#         return True
+#     if s[0] != s[-1]:
+#         return False
+#     return pal(s[1:-1])
+# 
+# print("True" if pal(s) else "False")
+
+
+
+# s = int(input())
+# l = 10 ** (len(str(s)) - 1)
+# 
+# def pal(s, l):
+#     if s <= 9:
+#         return True
+#     if s // l != s % 10:
+#         return False
+#     else:
+#         s = (s % l) // 10
+#         return pal(s, l // 100)
+# 
+# print("True" if pal(s, l) else "False")
+
+a=int(input())
+b=int(input())
+
+a=b+a
+b=a-b
+a=a-b
+
+print(a,b)
